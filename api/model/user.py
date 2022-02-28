5df86c2a5cbeda0578c627fbcd0eb3195a381154
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2022-02-28 23:23:08 +0100

    Add User field for confirmation state

--- api/model/user.py
+++ api/model/user.py
@@ -4,12 +4,14 @@ from database import Database
 
 db = Database.get_instance()
 
+
 @dataclass
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
     token = db.Column(db.String, unique=True)
+    confirmed_registration = db.Column(db.Boolean, unique=False, default=False) # TODO: check if default works
 
     def __init__(self, email, password) -> None:
         self.email = email
