2e8cfab7d868506a94032a91d9d3a93114120e4c
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2022-04-07 16:42:15 +0200

    Add create table route

--- api/main.ts
+++ api/main.ts
@@ -1,13 +1,19 @@
 import { Application, Context } from "../deps.ts";
 import Repository from "./repository.ts";
 
+type CreateTablesBody = { tables: Array<string> }
+
 const app = new Application();
 const repository: Repository = await Repository.getInstance();
 
 app.static("/", "./public");
 app.start({ port: 5000 });
 
-
 app.get("/", async (ctx: Context) => {
   return await ctx.file("./public/index.html");
+});
+
+app.post("/create-tables", async (ctx: Context) => {
+  const { tables } = await ctx.body as CreateTablesBody;
+  repository.createTables(...tables);
 });
