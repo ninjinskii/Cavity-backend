2f717b73eac23f6ac52eba5ba79aa116cc42fb3a
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2022-04-07 11:39:28 +0200

    Setup index.html

--- api/main.ts
+++ api/main.ts
@@ -1,5 +1,10 @@
-import { Application } from "../deps.ts";
+import { Application, Context } from "../deps.ts";
 
 const app = new Application();
 
+app.static("/", "./public");
 app.start({ port: 5000 });
+
+app.get("/", async (ctx: Context) => {
+  return await ctx.file("./public/index.html");
+});
--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { Application } from "https://deno.land/x/abc@v1.3.3/mod.ts";
+export { Application, Context } from "https://deno.land/x/abc@v1.3.3/mod.ts";
