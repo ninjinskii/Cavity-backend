0da7e935bdb98c7fba5aed09c45ed3ceb46ef969
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2022-04-08 11:42:29 +0200

    Add dynamic language switch support

--- api/main.ts
+++ api/main.ts
@@ -1,4 +1,9 @@
 import { Application, Context } from "../deps.ts";
+import AccountController from "./controller/account.ts";
+import AuthController from "./controller/auth.ts";
+import DataController from "./controller/data.ts";
+import ControllerManager from "./controller/manager.ts";
+import { EnTranslations, FrTranslations } from "./i18n/translatable.ts";
 import { Account, AccountDTO } from "./model/account.ts";
 import Repository from "./repository.ts";
 
@@ -8,7 +13,25 @@ type DeleteTableBody = { table: string };
 const app = new Application();
 const repository: Repository = await Repository.getInstance();
 
+const accountController = new AccountController(app, repository);
+const authController = new AuthController(app, repository);
+const dataController = new DataController(app, repository);
+const manager = new ControllerManager();
+manager.addControllers(accountController, authController, dataController);
+
 app.static("/", "./public");
+app.use((next) =>
+  (ctx: Context) => {
+    const language = ctx.request.headers.get("Accept-Language");
+    const translator = language?.includes("fr-")
+      ? new FrTranslations()
+      : new EnTranslations();
+
+    manager.updateControllersTranslator(translator);
+
+    return next(ctx);
+  }
+);
 app.start({ port: 5000 });
 
 app.get("/", async (ctx: Context) => {
