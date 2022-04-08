5d7d8878fc277b1b986adb29fb4b6973f03e45be
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2022-04-08 17:59:55 +0200

    Wip smtp client

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
 export { Application, Context } from "https://deno.land/x/abc@v1.3.3/mod.ts";
 export { Client as PostgresClient } from "https://deno.land/x/postgres@v0.15.0/mod.ts";
 export { QueryObjectResult } from "https://deno.land/x/postgres@v0.15.0/query/query.ts";
+export { SmtpClient } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
