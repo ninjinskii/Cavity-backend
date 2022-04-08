ea1b7b66f500e7eb578dc43f1d6117f13ef2dab6
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2022-04-08 13:35:05 +0200

    Add translations

--- api/i18n/translatable.ts
+++ api/i18n/translatable.ts
@@ -2,6 +2,7 @@ export interface Translatable {
   fullLocaleName: string;
   baseError: string; // Used when an error is truly unknown or when the error does not depend on the user.
   notFound: string;
+  accountAlreadyExists: string;
   // accountResgistrationFailed: string
 }
 
@@ -9,10 +10,12 @@ export class FrTranslations implements Translatable {
   fullLocaleName = "Français";
   baseError = "Une erreur est survenue.";
   notFound = "Objet non trouvé.";
+  accountAlreadyExists = "Le compte existe déjà.";
 }
 
 export class EnTranslations implements Translatable {
   fullLocaleName = "English";
   baseError = "An error occured.";
   notFound = "This object does not exists.";
+  accountAlreadyExists = "Account already exists.";
 }
