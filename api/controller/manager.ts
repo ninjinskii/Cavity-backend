378c42ecd1405e032252178e3b4e3fc15d8c7def
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2022-04-08 12:39:57 +0200

    Fix worng manager impl

--- api/controller/manager.ts
+++ api/controller/manager.ts
@@ -5,7 +5,7 @@ export default class ControllerManager {
   controllers: Array<Controller> = [];
 
   addControllers(...controllers: Array<Controller>): void {
-    controllers.push(...controllers);
+    this.controllers.push(...controllers);
   }
 
   updateControllersTranslator(translator: Translatable): void {
