ac266c51b601e8273498afd3752fa434d89acf9c
ninjinski <louis.bailly1@etu.univ-lorraine.fr>
2022-04-11 08:09:13 +0200

    County controller wip to test auth

--- api/controller/data.ts
+++ api/controller/data.ts
@@ -1,6 +1,29 @@
+import { Context, jwt } from "../../deps.ts";
+import { County, CountyDTO } from "../model/county.ts";
 import Controller from "./controller.ts";
 
 export default class DataController extends Controller {
+  get county() {
+    return "/county";
+  }
+
   handleRequests(): void {
+    this.app
+      .post(this.county, async (ctx: Context) => this.postCounty(ctx))
+      .get(this.county, async (ctx: Context) => this.getCounties(ctx))
+      .get(`${this.county}/:id`, async (ctx: Context) => this.getCounty(ctx));
+  }
+
+  async postCounty(ctx: Context): Promise<void> {
+    const authorization = ctx.request.headers.get("Authorization");
+
+    if (!authorization) {
+      return;
+    }
+
+    const [_, token] = authorization.split(" ");
+    const payload = await jwt.verify(token, "");
+    const countyDto = await ctx.body as CountyDTO;
+    const county = new County(countyDto, 1);
   }
 }
