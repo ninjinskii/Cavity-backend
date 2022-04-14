627007e1eebf7ad5e0c2b7567e6784a115c8cd9f
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2022-04-14 18:07:11 +0200

    Add POST version

--- api/controller/data.ts
+++ api/controller/data.ts
@@ -17,11 +17,51 @@ export default class DataController extends Controller {
 
   handleRequests(): void {
     this.app
-      // .post(
-      //   this.county,
-      //   async (ctx: Context) => this.genericPost(ctx, "county", "County"),
-      // )
+      .post(this.county, (ctx: Context) => this.handlePost(ctx))
       .get(this.county, (ctx: Context) => this.handleGet(ctx));
+  }
+
+  async handlePost(ctx: Context): Promise<void> {
+    const authorization = ctx.request.headers.get("Authorization");
+
+    if (!authorization) {
+      return ctx.json({ message: this.translator.unauthorized }, 401);
+    }
+
+    const [_, token] = authorization.split(" ");
+
+    try {
+      const { account_id } = await jwt.verify(token, this.jwtKey) as {
+        account_id: string;
+      };
+      const accountId = parseInt(account_id);
+
+      const dtoList = await ctx.body;
+
+      if (!(dtoList instanceof Array)) {
+        return ctx.json({ message: this.translator.missingParameters }, 400);
+      }
+
+      const objects = dtoList.map((dto) =>
+        mapper[ctx.path].fromDTO(dto, accountId)
+      );
+
+      try {
+        await this.repository.insertMany(mapper[ctx.path].table, objects);
+        // await this.repository.doInTransaction(
+        //   `postCounty-${account_id}`,
+        //   async () => {
+        //     //await this.repository.deleteBy("county", "account_id", account_id);
+        //   },
+        // );
+        return ctx.json({ ok: true });
+      } catch (error) {
+        console.warn("Unable to insert objects");
+        // error 400
+      }
+    } catch (error) {
+      return ctx.json({ message: this.translator.unauthorized }, 401);
+    }
   }
 
   async handleGet(ctx: Context): Promise<void> {
@@ -62,9 +102,17 @@ export default class DataController extends Controller {
 // and add a super class that require a table name and a function to transform
 // their objects to DTOs.
 interface PathMapper {
-  [path: string]: { table: string; toDTO: (object: any) => any };
+  [path: string]: {
+    table: string;
+    toDTO: (object: any) => any;
+    fromDTO: (dto: any, account_id: number) => any;
+  };
 }
 
 const mapper: PathMapper = {
-  "/county": { table: "county", toDTO: County.toDTO },
+  "/county": {
+    table: "county",
+    toDTO: County.toDTO,
+    fromDTO: (dto, accountId) => new County(dto, accountId),
+  },
 };
