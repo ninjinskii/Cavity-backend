f377fee6444a559473e3892ff8796672e08fdd0c
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2022-05-11 13:55:57 +0200

    Remove double PK

--- api/model/bottle.ts
+++ api/model/bottle.ts
@@ -3,14 +3,15 @@ import { DataTypes, Model } from "../../deps.ts";
 export class Bottle extends Model{
   static table = "account";
   static fields = {
-    id: {
+    _id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
+      autoIncrement: true,
     },
+    id: DataTypes.INTEGER,
     accountId: {
       type: DataTypes.INTEGER,
       as: "account_id",
-      primaryKey: true,
     },
     wineId: {
       type: DataTypes.INTEGER,
