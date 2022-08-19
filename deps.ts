ee6018b1bd06f788d3b06acf125cc7f1a688b9a9
ninjinski <louis.bailly.pro@gmail.com>
2022-08-19 15:15:21 +0200

    Fix depedency issue

--- deps.ts
+++ deps.ts
@@ -1,9 +1,8 @@
-// export { Application, Context } from "https://deno.land/x/abc@v1.3.3/mod.ts";
-export { Application, Router, Context, Status } from "https://deno.land/x/oak@v10.6.0/mod.ts";
-export { getQuery } from "https://deno.land/x/oak@v10.6.0/helpers.ts";
-export { Client as PostgresClient, Transaction } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
-export { QueryObjectResult } from "https://deno.land/x/postgres@v0.16.1/query/query.ts";
-export { Database as Client, PostgresConnector, Model, DataTypes } from "https://raw.githubusercontent.com/joeldesante/denodb/master/mod.ts";
+export { Application, Router, Context, Status } from "https://deno.land/x/oak@v10.5.1/mod.ts";
+export { getQuery } from "https://deno.land/x/oak@v10.5.1/helpers.ts";
+export { Client as PostgresClient, Transaction } from "https://deno.land/x/postgres@v0.15.0/mod.ts";
+export { Database as Client, PostgresConnector, Model, DataTypes } from "https://raw.githubusercontent.com/ninjinskii/denodb/master/mod.ts";
+export { QueryObjectResult } from "https://deno.land/x/postgres@v0.15.0/query/query.ts";
 export * as jwt from "https://deno.land/x/djwt@v2.7/mod.ts";
 export * as bcrypt from "https://deno.land/x/bcrypt@v0.4.0/mod.ts";
 export * as logger from "https://deno.land/std@0.152.0/log/mod.ts";
