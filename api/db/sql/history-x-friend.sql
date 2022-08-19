a9d3686b208cdab676c78550036c0f2d45794775
ninjinski <louis.bailly.pro@gmail.com>
2022-08-19 17:39:28 +0200

    Add sql scripts back (need to adapt those)

---
+++ api/db/sql/history-x-friend.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS history_x_friend (
+  _id serial PRIMARY KEY,
+  account_id INT NOT NULL,
+  history_entry_id INT NOT NULL,
+  friend_id INT NOT NULL,
+  UNIQUE (account_id, history_entry_id, friend_id)
+);
