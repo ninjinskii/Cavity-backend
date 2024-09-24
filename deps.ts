9a7da878af8ca6d5e9e4a6bfad6b080adf5a3ce1
ninjinski <louis.bailly.pro@gmail.com>
2024-09-24 18:48:07 +0200

    chore(deps): update deno & deps

--- deps.ts
+++ deps.ts
@@ -4,13 +4,12 @@ export {
   Router,
   Status,
   testing,
-} from "https://deno.land/x/oak@v10.6.0/mod.ts";
-export type { BodyJson } from "https://deno.land/x/oak@v10.6.0/mod.ts";
-export { createMockContext } from "https://deno.land/x/oak@v10.6.0/testing.ts";
-export { getQuery } from "https://deno.land/x/oak@v10.6.0/helpers.ts";
-export * as jwt from "https://deno.land/x/djwt@v2.7/mod.ts";
-export * as bcrypt from "https://deno.land/x/bcrypt@v0.4.0/mod.ts";
-export * as logger from "https://deno.land/std@0.141.0/log/mod.ts";
+} from "https://deno.land/x/oak@v17.0.0/mod.ts";
+export type { BodyJson } from "https://deno.land/x/oak@v17.0.0/mod.ts";
+export { createMockContext } from "https://deno.land/x/oak@v17.0.0/testing.ts";
+export * as jwt from "https://deno.land/x/djwt@v3.0.2/mod.ts";
+export * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
+export * as logger from "https://deno.land/std@0.224.0/log/mod.ts";
 export { snakeCase, camelCase } from "https://deno.land/x/case@2.2.0/mod.ts";
 export {
   assertSpyCall,
@@ -18,17 +17,17 @@ export {
   returnsNext,
   spy,
   stub,
-} from "https://deno.land/std@0.196.0/testing/mock.ts";
-export type { Spy, Stub } from "https://deno.land/std@0.196.0/testing/mock.ts";
+} from "https://deno.land/std@0.224.0/testing/mock.ts";
+export type { Spy, Stub } from "https://deno.land/std@0.224.0/testing/mock.ts";
 export {
   assertEquals,
   assertNotEquals,
-} from "https://deno.land/std@0.130.0/testing/asserts.ts";
+} from "https://deno.land/std@0.224.0/testing/asserts.ts";
 export {
   afterAll,
   beforeEach,
   describe,
   it,
-} from "https://deno.land/std@0.196.0/testing/bdd.ts";
+} from "https://deno.land/std@0.224.0/testing/bdd.ts";
 export { createClient, SupabaseClient } from "jsr:@supabase/supabase-js@2.44.3";
 export * as Sentry from "https://deno.land/x/sentry@7.107.0/index.mjs";
